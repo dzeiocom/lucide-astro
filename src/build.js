f84fcc1634c48d0313c480ea6698c1622343c52b
Florian BOUILLON <f.bouillon@aptatio.com>
2023-07-03 10:56:52 +0200

    feat: Optimize to reduce package final size (~50%)
    Signed-off-by: Florian BOUILLON <f.bouillon@aptatio.com>

--- src/build.js
+++ src/build.js
@@ -1,65 +1,51 @@
 import fs from 'fs/promises'
 import Lucide from "lucide-static"
 
-const componentTemplate = (icon) => `---
-export interface Props {
-	size?: number
-	width?: number
-	height?: number
-	strokeWidth?: number
-	stroke?: string
-	fill?: string
-	class?: string
-	viewBox?: string
-	strokeLinecap?: "round" | "butt" | "square" | "inherit"
-	strokeLinejoin?: "round" | "inherit" | "miter" | "bevel"
-}
-
-const {
-	size = 24,
-	strokeWidth = 2,
-	width = size,
-	height = size,
-	stroke = 'currentColor',
-	strokeLinecap = 'round',
-	strokeLinejoin = 'round',
-	fill = 'none',
-	viewBox = '0 0 24 24'
-} = Astro.props
-
----
-
-<svg
-	xmlns="http://www.w3.org/2000/svg"
-	class={Astro.props.class}
-	width={width}
-	height={height}
-	fill={fill}
-	viewBox={viewBox}
-	stroke={stroke}
-	stroke-width={strokeWidth}
-	stroke-linecap={strokeLinecap}
-	stroke-linejoin={strokeLinejoin}
->
-	${icon.replace(/<svg(?:.|\n)*?>((?:.|\n)*)<\/svg>/gm, '$1').replace(/  /g, '\t').trim()}
-</svg>
-`
-
+// reset the dist folder
 await fs.rm('./dist', { recursive: true, force: true})
 await fs.mkdir('./dist')
 
+// get the template
+const template = await fs.readFile('./src/Template.astro', 'utf8')
+
+// funciton tht fill the template with the icon SVG
+function fillTemplate(icon) {
+	return template.replace(
+		'<!-- icon -->',
+		icon.replace(/<svg(?:.|\n)*?>((?:.|\n)*)<\/svg>/gm, '$1').replace(/  /g, '\t').trim()
+	)
+}
+
+// copy the base layout
+await fs.copyFile('./src/.Layout.astro', './dist/.Layout.astro')
+
+// get the base index.d.ts file
+const inter = await fs.readFile('./src/index.d.ts', 'utf8')
+
 try {
+	// ceate the index
 	let index = ''
+
+	// loop through each icons
 	for (const name in Lucide) {
+		// get its SVG
 		const icon = Lucide[name]
+
+		// make the name PascalCase
 		const fullName = name.slice(0, 1).toUpperCase() + name.slice(1)
+
+		// get the file path
 		const filePath = `./dist/${fullName}.astro`;
-		// await fs.ensureDir(path.dirname(filePath));
-		await fs.writeFile(filePath, componentTemplate(icon), "utf-8");
+
+		// compile the icon and write it out
+		await fs.writeFile(filePath, fillTemplate(icon), "utf-8");
+
+		// add the icon to the index
 		index += `export { default as ${fullName} } from './${fullName}.astro'\n`
 	}
+	// write the index to the js and d.ts files
 	await fs.writeFile('./dist/index.js', index, 'utf-8')
-	await fs.writeFile('./dist/index.d.ts', index, 'utf-8')
+	await fs.writeFile('./dist/index.d.ts', inter + index, 'utf-8')
 } catch (e) {
 	console.error('Error building the Library', e)
 }
---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+export interface Props {
+	size?: number
+	width?: number
+	height?: number
+	strokeWidth?: number
+	stroke?: string
+	fill?: string
+	class?: string
+	viewBox?: string
+	strokeLinecap?: "round" | "butt" | "square" | "inherit"
+	strokeLinejoin?: "round" | "inherit" | "miter" | "bevel"
+}
